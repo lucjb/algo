301e7a09484ca1d6ee6764981311682328503617
cronopio <lucejb@gmail.com>
2014-03-29 11:22:38 -0300

    added problems statements

--- src/org/cronopios/training/codility/sorting/MaxProductOfThree.java
+++ src/org/cronopios/training/codility/sorting/MaxProductOfThree.java
@@ -1,20 +1,40 @@
 package org.cronopios.training.codility.sorting;
+
+/**
+ * A non-empty zero-indexed array A consisting of N integers is given. The
+ * product of triplet (P, Q, R) equates to A[P] * A[Q] * A[R] (0 ≤ P < Q < R <
+ * N). For example, array A such that: A[0] = -3 A[1] = 1 A[2] = 2 A[3] = -2
+ * A[4] = 5 A[5] = 6 contains the following example triplets: (0, 1, 2), product
+ * is −3 * 1 * 2 = −6 (1, 2, 4), product is 1 * 2 * 5 = 10 (2, 4, 5), product is
+ * 2 * 5 * 6 = 60 Your goal is to find the maximal product of any triplet. Write
+ * a function: class Solution { public int solution(int[] A); } that, given a
+ * non-empty zero-indexed array A, returns the value of the maximal product of
+ * any triplet. For example, given array A such that: A[0] = -3 A[1] = 1 A[2] =
+ * 2 A[3] = -2 A[4] = 5 A[5] = 6 the function should return 60, as the product
+ * of triplet (2, 4, 5) is maximal. Assume that: N is an integer within the
+ * range [3..100,000]; each element of array A is an integer within the range
+ * [−1,000..1,000]. Complexity: expected worst-case time complexity is
+ * O(N*log(N)); expected worst-case space complexity is O(1), beyond input
+ * storage (not counting the storage required for input arguments). Elements of
+ * input arrays can be modified.
+ * 
+ * @author 1
+ * 
+ */
 public class MaxProductOfThree {
 
 	public static void main(String[] args) {
-		int solution = new MaxProductOfThree().solution(new int[] { 1, -8, -1, -500,
-				-2, -3, -4, -100, -100 });
+		int solution = new MaxProductOfThree().solution(new int[] { 1, -8, -1, -500, -2, -3, -4, -100, -100 });
 		System.out.println(solution);
 	}
 
 	/**
 	 * linear solution
+	 * 
 	 * @param A
 	 * @return
 	 */
 	public int solution(int[] A) {
-		// write your code in Java SE 7
-
 		int firstMax = -1;
 		for (int i = 0; i < A.length; i++) {
 			if (firstMax == -1 || A[i] > A[firstMax]) {
@@ -31,8 +51,7 @@ public class MaxProductOfThree {
 
 		int thirdMax = -1;
 		for (int i = 0; i < A.length; i++) {
-			if (i != firstMax && i != secondMax
-					&& (thirdMax == -1 || A[i] > A[thirdMax])) {
+			if (i != firstMax && i != secondMax && (thirdMax == -1 || A[i] > A[thirdMax])) {
 				thirdMax = i;
 			}
 		}
@@ -46,14 +65,12 @@ public class MaxProductOfThree {
 
 		int seccondMin = -1;
 		for (int i = 0; i < A.length; i++) {
-			if (i != firstMax && i != firstMin
-					&& (seccondMin == -1 || A[i] < A[seccondMin])) {
+			if (i != firstMax && i != firstMin && (seccondMin == -1 || A[i] < A[seccondMin])) {
 				seccondMin = i;
 			}
 		}
 
-		return (int) Math.max(A[firstMax] * A[secondMax] * A[thirdMax],
-				A[firstMax] * A[firstMin] * A[seccondMin]);
+		return (int) Math.max(A[firstMax] * A[secondMax] * A[thirdMax], A[firstMax] * A[firstMin] * A[seccondMin]);
 
 	}
 }
